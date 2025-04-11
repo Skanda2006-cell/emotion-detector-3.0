06c95b3b9e4159ebfe454ccfb5ea38c5d533c8f8
Skanda2006-cell <skanda092006@gmail.com>
2025-04-11 17:57:16 +0530

    Update emotion_detector_3.0.py

--- emotion_detector_3.0.py
+++ emotion_detector_3.0.py
@@ -17,7 +17,7 @@ emotion_gifs = {
     "joy": "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif",
     "sadness": "https://media.giphy.com/media/d2lcHJTG5Tscg/giphy.gif",
     "anger": "https://media.giphy.com/media/l3V0j3ytFyGHqiV7W/giphy.gif",
-    "fear": "https://media.giphy.com/media/3o6ZsYwMl7UE8cF7Vu/giphy.gif",
+    "fear": "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmsyOGRqMm1lcXg3Zjd5bW9uc3hzbno0YmJodDR3MGszdHRwbTZ0dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DHw6uxU2WbJ3a/giphy.gif",
     "surprise": "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif",
     "love": "https://media.giphy.com/media/l0HlOvJ7yaacpuSas/giphy.gif",
     "neutral": "https://media.giphy.com/media/xT9IgIc0lryrxvqVGM/giphy.gif",
