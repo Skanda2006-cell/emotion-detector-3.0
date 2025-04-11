2a554b281c622aa4c0450b4f5e0e03357d65bda4
Skanda2006-cell <skanda092006@gmail.com>
2025-04-11 17:40:48 +0530

    Update emotion_detector_3.0.py

--- emotion_detector_3.0.py
+++ emotion_detector_3.0.py
@@ -14,7 +14,7 @@ emotion_colors = {
     "neutral": "#cfd8dc", "disgust": "#dcedc8"
 }
 emotion_gifs = {
-    "joy": "https://media.giphy.com/media/yoJC2Olx0ekMy2nX7W/giphy.gif",
+    "joy": "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif",
     "sadness": "https://media.giphy.com/media/d2lcHJTG5Tscg/giphy.gif",
     "anger": "https://media.giphy.com/media/l3V0j3ytFyGHqiV7W/giphy.gif",
     "fear": "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif",
